ffaf7cd7b94e5476508be9dbeff58f734063fa56
unknown <sudiptapan98@gmail.com>
2022-06-04 11:16:08 +0530

    add toast message

--- app/src/main/java/com/example/gitprojecttest/MainActivity.java
+++ app/src/main/java/com/example/gitprojecttest/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.gitprojecttest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button buttontest;
@@ -14,6 +16,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         buttontest=findViewById(R.id.buttontest);
+        buttontest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getApplicationContext(),"button click",Toast.LENGTH_LONG).show();
+            }
+        });
 
 
     }
